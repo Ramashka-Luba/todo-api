1d88bdc4fbc2769a3acb7f73f1e540839e9a4ce0
Ramashka Luba <lromashko911@gmail.com>
2022-11-16 13:48:15 +0300

    fixed styles

--- src/components/page/registrationPage/RegistrationPage.jsx
+++ src/components/page/registrationPage/RegistrationPage.jsx
@@ -6,7 +6,7 @@ import Arrow from "./../../../assets/arrow-right.png"
 
 const RegistrationPage = () => {
 
-    const [data,setData] = useState({
+    const [data, setData] = useState({
         name: "",
         username: "",
         email: "",
@@ -35,11 +35,11 @@ const RegistrationPage = () => {
         });
     };
 
-    const heandlePost = async() => {
+    const heandlePost = async () => {
         console.log("-----heandlePost-------");
         console.log(data);
         try {
-            const res = await axios.post ("https://first-node-js-app-r.herokuapp.com/api/users/register", data );
+            const res = await axios.post("https://first-node-js-app-r.herokuapp.com/api/users/register", data);
             console.log(res);
 
             navigate('/loginPage'); //вызываем navigate и передаем url, куда хотим чтоб нас перекинуло
@@ -50,71 +50,102 @@ const RegistrationPage = () => {
     };
 
 
+    // ----------------Validations-----------------
+
+// const useInput = (initialValue, validations) => {
+//     const [value, setValue] = useState (initialValue); // значение внутри Inputa
+//     const [isDirty, setDirty] = useState (false);
+
+//     const onChange = (e) => {
+//         setValue(e.target.value)
+//     }
+
+//     const onBlur = (e) => {
+//         setDirty(true)
+//     }
+//     return {
+//         value,
+//         onChange,
+//         onBlur
+//     }
+// }
+
+
+
+
+    // ----------------Validations-----------------
+
+
 
     return (
         <div className={s.RegistrationPage}>
             <div className={s.inner}>
                 <h2 className={s.title}>Registration</h2>
 
-                <form onSubmit={(e) => handleSubmit(e)} 
-                        className={s.wrapForm}>
+                <form onSubmit={(e) => handleSubmit(e)}
+                    className={s.wrapForm}>
 
                     <div className={s.wrapInput}>
-                        <label className={s.label} htmlFor="name">Name<span>*</span></label>
-                        <input 
-                            onChange={(e) => setData({...data, name : e.target.value})}
-                            value={data.name} 
+                        <label className={s.label} htmlFor="name">Name
+                            <span>*</span>
+                            <span className={s.error}>Name is required</span>
+                        </label>
+
+                        <input
+                            onChange={(e) => setData({ ...data, name: e.target.value })}
+                            value={data.name}
                             type='text'
-                            className={s.input}/>
+                            className={s.input} />
+
                     </div>
 
                     <div className={s.wrapInput}>
                         <label className={s.label} htmlFor="username">Username<span>*</span></label>
-                        <input 
-                            onChange={(e) => setData({...data, username : e.target.value})}
-                            value={data.username} 
+                        <input
+                            onChange={(e) => setData({ ...data, username: e.target.value })}
+                            value={data.username}
                             type='text'
-                            className={s.input}  />
+                            className={s.input} />
                     </div>
 
                     <div className={s.wrapInput}>
                         <label className={s.label} htmlFor="email">Email<span>*</span></label>
                         <input
-                            onChange={(e) => setData({...data, email : e.target.value})}
-                            value={data.email} 
+                            onChange={(e) => setData({ ...data, email: e.target.value })}
+                            value={data.email}
                             type='email'
-                            className={s.input}  />
+                            className={s.input} />
                     </div>
 
                     <div className={s.wrapInput}>
                         <label className={s.label} htmlFor="password">Password<span>*</span></label>
-                        <input 
-                            onChange={(e) => setData({...data, password : e.target.value})}
-                            value={data.password} 
+                        <input
+                            onChange={(e) => setData({ ...data, password: e.target.value })}
+                            value={data.password}
                             type='password'
-                        className={s.input}  />
+                            className={s.input} />
                     </div>
 
                     <div className={s.wrapInput}>
                         <label className={s.label} htmlFor="gender">Gender<span>*</span></label>
                         <div className={s.select}>
-                            <select 
-                                onChange={(e) => setData({...data, isMan : !data.isMan})}
-                                value={data.isMan} 
+                            <select
+                                onChange={(e) => setData({ ...data, isMan: !data.isMan })}
+                                value={data.isMan}
                                 name="gender">
-                                    <option value="true">Male</option>
-                                    <option value="false">Female</option>
+                                <option value="true">Male</option>
+                                <option value="false">Female</option>
                             </select>
                         </div>
                     </div>
 
                     <div className={s.wrapInput}>
                         <label className={s.label} htmlFor="age">Age<span>*</span></label>
-                        <input 
-                            onChange={(e) => setData({...data, age : Number(e.target.value)})}
-                            value={data.age} 
+                        <input
+                            onChange={(e) => setData({ ...data, age: Number(e.target.value) })}
+                            value={data.age}
                             type='number'
-                            className={s.input}  />
+                            className={s.input} />
                     </div>
 
                     <div className={s.wrapBtn}>
